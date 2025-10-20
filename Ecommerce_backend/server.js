8ad6b59a351fc8a93a77bec134e7ae78465493c4
MinahilMustafa <161214527+MinahilMustafa@users.noreply.github.com>
2025-10-20 22:14:24 +0500

    Remove authRoutes from server.js
    Removed authentication routes from server setup.

--- Ecommerce_backend/server.js
+++ Ecommerce_backend/server.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import userRoutes from "./routes/userRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
@@ -20,7 +19,6 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
